7ff436f0ce3bd3108a3f18fd964ee347a3f8e616
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-09-21 20:38:14 +0300

    Add more middlewares

--- 2109/serv/index.js
+++ 2109/serv/index.js
@@ -4,9 +4,28 @@ const PORT = 4321;
 const app = express(); //God object, implements event emitter
 
 app
-  .all('/', req => req.res.send('Hello there')) //вызываем любым глаголом хттп
+  .use((req, res, next) => console.log(req.url) || next())
+  // console.log возвращает undefined, так что можно его выполнить
+  // и потом через || прокинуть выполнение к вызову next()
+  // нужно обязательно вызывать res.send() или next(), а то всё повесится
+  // после вызова next() или res.send() бессмысленно что-то писать в этом мидлвэре
+  .get('/', req => req.res.send('Hello there'))
   .get('/web', req => req.res.send('Ohh awesome'))
   //объект в первом аргументе маршрутизатора уже содержится в первом аргументе,
   // так что можно получать доступ к response через request
   .get('/num', req => req.res.send(404)) //число воспринимается как статус-код
+  .get('/fup', res => {
+    throw new Error('imma dyin')
+  })
+  .use(req => req
+    .res
+    .set({'Content-Type': 'text/html; charset=utf-8'})
+    .status(404)
+    .end('<h1>Nope.</h1>')
+  )
+  .use((e, req, res, next) => res //этот мидлвэр сработает при переходе по проблемному маршруту
+    .set({'Content-Type': 'text/html; charset=utf-8'})
+    .status(500)
+    .end('imma fuckin dead.')
+  )
   .listen(process.env.PORT || PORT, () => console.log('It works')); //берем из среды, а если нет - наш порт
