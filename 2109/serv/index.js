ac5c43c90ab33bf3c34189074d48450455613627
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-09-21 20:12:32 +0300

    Add routes

--- 2109/serv/index.js
+++ 2109/serv/index.js
@@ -4,8 +4,9 @@ const PORT = 4321;
 const app = express(); //God object, implements event emitter
 
 app
-  .get('/', req => req.res.send('Hello there'))
-  .get('/web', req => req.res.send('Hello there'))
+  .all('/', req => req.res.send('Hello there')) //вызываем любым глаголом хттп
+  .get('/web', req => req.res.send('Ohh awesome'))
   //объект в первом аргументе маршрутизатора уже содержится в первом аргументе,
   // так что можно получать доступ к response через request
+  .get('/num', req => req.res.send(404)) //число воспринимается как статус-код
   .listen(process.env.PORT || PORT, () => console.log('It works')); //берем из среды, а если нет - наш порт
