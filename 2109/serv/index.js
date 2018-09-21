08c218e89bb1b15af13fab13198d27c8af53a55d
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-09-21 20:04:09 +0300

    Add simple express server

--- 2109/serv/index.js
+++ 2109/serv/index.js
@@ -1,9 +1,11 @@
-const e = require('events');
+const express = require('express');
+const PORT = 4321;
 
-const myEm = new e();
-myEm.on('myEvent', () => {
-  console.log('Hellooo there');
-}); //в одном модуле эмитим
+const app = express(); //God object, implements event emitter
 
-myEm.emit('myEvent'); // а в каком-нибудь другом модуле реагируем
-// при некоторых определенных условиях, асинхронно
+app
+  .get('/', req => req.res.send('Hello there'))
+  .get('/web', req => req.res.send('Hello there'))
+  //объект в первом аргументе маршрутизатора уже содержится в первом аргументе,
+  // так что можно получать доступ к response через request
+  .listen(process.env.PORT || PORT, () => console.log('It works')); //берем из среды, а если нет - наш порт
